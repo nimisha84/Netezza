ada8f59db4090d2581954ce563b67d12804c06c3
nimisha Shrivastava <nimisha_shrivastava@intuit.com>
2015-09-17 15:45:57 +0530

    netezza code commit

---
+++ AWSTest1_Add_your_S3_creds/AWSTest1/Program1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using System.IO;
+
+namespace AWSTest1
+{
+    class Program
+    {
+        static string existingBucketName = "*** Provide bucket name ***";
+        static string keyName = "*** Provide your object key ***";
+        static string filePath = "*** Provide file name ***";
+
+        static void Main(string[] args)
+        {
+            try
+            { 
+                TransferUtility fileTransferUtility = new
+                    TransferUtility(new AmazonS3Client(Amazon.RegionEndpoint.USEast1));
+
+                // 1. Upload a file, file name is used as the object key name.
+                fileTransferUtility.Upload(filePath, existingBucketName);
+                Console.WriteLine("Upload 1 completed");
+
+                // 2. Specify object key name explicitly.
+                fileTransferUtility.Upload(filePath,
+                                          existingBucketName, keyName);
+                Console.WriteLine("Upload 2 completed");
+
+                // 3. Upload data from a type of System.IO.Stream.
+                using (FileStream fileToUpload =
+                    new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    fileTransferUtility.Upload(fileToUpload,
+                                               existingBucketName, keyName);
+                }
+                Console.WriteLine("Upload 3 completed");
+
+                // 4.Specify advanced settings/options.
+                TransferUtilityUploadRequest fileTransferUtilityRequest = new TransferUtilityUploadRequest
+                {
+                    BucketName = existingBucketName,
+                    FilePath = filePath,
+                    StorageClass = S3StorageClass.ReducedRedundancy,
+                    PartSize = 5242880, // 5 MB.
+                    Key = keyName,
+                    CannedACL = S3CannedACL.PublicRead
+                };
+                fileTransferUtilityRequest.Metadata.Add("param1", "Value1");
+                fileTransferUtilityRequest.Metadata.Add("param2", "Value2");
+                fileTransferUtility.Upload(fileTransferUtilityRequest);
+                Console.WriteLine("Upload 4 completed");
+            }
+            catch (AmazonS3Exception s3Exception)
+            {
+                Console.WriteLine(s3Exception.Message,
+                                  s3Exception.InnerException);
+            }
+        
+
+
+
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+
+       
+
+
+    }
+}
